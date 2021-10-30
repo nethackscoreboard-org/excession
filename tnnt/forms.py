75f995259531ea26caf194e6808f72b6393039e6
copperwater <aosdict@gmail.com>
2021-10-30 14:00:08 -0400

    Close off a few exploits in user-nameable clans
    1. You could set your clan name to a URL fragment that would trigger
       whenever someone navigated to /clan/<clanname>. Example: a clan named
       "../logout" would instead log out whoever navigated to that clan's
       page. Fix this by disallowing slashes in clan names.
    2. You could set your clan name with a ton of diacritics that could
       stack and crash into other clans in clans lists and other parts of
       the site in general. Fix this by disallowing more than one
       consecutive combining character in the clan name.
    3. You could set your clan name to "\", which is perfectly valid but
       would break when trying to navigate to /clan/<clanname>. Fix this by
       urlencoding all clan names in the clink template.

--- tnnt/forms.py
+++ tnnt/forms.py
@@ -1,7 +1,32 @@
 from django import forms
+import unicodedata
 
 class CreateClanForm(forms.Form):
     clan_name = forms.CharField(max_length = 127, label='Create a clan:')
+
+    # Custom validator for the clan_name field. Enforces some constraints we
+    # don't want to allow in clan names.
+    def clean_clan_name(self):
+        data = self.cleaned_data['clan_name']
+
+        # refuse slashes, since this will mess up routing to /clan/<clanname>
+        # endpoints
+        # (post 2021 TODO: we could probably improve this by making the clan name
+        # a GET parameter rather than part of the URL)
+        if '/' in data:
+            raise forms.ValidationError('Clan names cannot contain slashes')
+
+        # allow diacritics, but only one combining character per regular character
+        # (otherwise ZALGO style clan names can be created which mess up other
+        # parts of the page)
+        prevcombine = False
+        for char in data:
+            nowcombine = unicodedata.combining(char)
+            if nowcombine and prevcombine:
+                raise forms.ValidationError('Clan names cannot have more than two consecutive combining characters')
+            prevcombine = nowcombine
+
+        return self.cleaned_data['clan_name']
 
 class InviteMemberForm(forms.Form):
     invitee = forms.CharField(max_length = 32, label='Invite:')
